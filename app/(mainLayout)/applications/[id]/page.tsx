995a3a78fd32a300a785d8140b31cc763c18f6b5
evansmunsha <evansensteen@gmail.com>
2025-04-04 15:39:40 +0200

    "/applications"

--- app/(mainLayout)/applications/[id]/page.tsx
+++ app/(mainLayout)/applications/[id]/page.tsx
@@ -141,7 +141,6 @@ export default function ApplicationDetailsPage({ params }: { params: Promise<{ i
         </Link>
         <h1 className="text-3xl font-bold mt-2">Application Details</h1>
       </div>
-      <ApplicationInsights jobId={jobId} />
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         <div className="md:col-span-2 space-y-6">
           <Card>
