4eebef6af80545b0aafe8cc0caeca06175c2a4f6
evansmunsha <evansensteen@gmail.com>
2025-05-23 02:13:30 +0200

    CREDIT_COSTS

--- app/utils/credits.ts
+++ app/utils/credits.ts
@@ -1,11 +1,7 @@
 //app/utils/credits.ts
 
 
-import { cookies } from "next/headers"
 import { prisma } from "./db"
-import { auth } from "./auth"
-
-
 
 // Define credit costs per feature
 export const CREDIT_COSTS = {
@@ -144,37 +140,3 @@ export function getFeatureCreditCost(feature: string): number {
   return CREDIT_COSTS[feature as keyof typeof CREDIT_COSTS] || 10
 }
 // utils/credits/handleCreditCharge.ts
-
-export const GUEST_CREDIT_COOKIE = "domijob_guest_credits"
-export const MAX_GUEST_CREDITS = 50
-
-export async function handleCreditCharge(featureType: string) {
-  const creditCost = CREDIT_COSTS[featureType as keyof typeof CREDIT_COSTS] || 5
-  const session = await auth()
-  const userId = session?.user?.id
-
-  if (userId) {
-    const userCredits = await getUserCredits(userId)
-    if (userCredits < creditCost) throw new Error("Insufficient credits.")
-    await deductCredits(userId, featureType)
-    return { userId, isGuest: false, creditsUsed: creditCost, remainingCredits: userCredits - creditCost }
-  }
-
-  const cookieStore = await cookies()
-  const cookie = cookieStore.get(GUEST_CREDIT_COOKIE)
-  let guestCredits = cookie ? parseInt(cookie.value) : MAX_GUEST_CREDITS
-  if (isNaN(guestCredits)) guestCredits = MAX_GUEST_CREDITS
-
-  if (guestCredits < creditCost) {
-    throw new Error("You've used all your free credits. Sign up to get 50 more free credits!")
-  }
-
-  const updatedCredits = guestCredits - creditCost
-  return {
-    userId: "guest",
-    isGuest: true,
-    creditsUsed: creditCost,
-    remainingCredits: updatedCredits,
-    guestCreditsToSet: updatedCredits, // include this to be used in API handler
-  }
-}
